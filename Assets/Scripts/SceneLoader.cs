968ef324baca31a6881d74d4c7d1c1b8e9ff8a96
eric2458 <59672148+eric2458@users.noreply.github.com>
2026-03-09 09:52:18 -0700

    Creditds scene started
    Made a main screen now working on credits

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+public class SceneLoader : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void LoadGame()
+    {
+        StartCoroutine(_LoadGame());
+
+        IEnumerator _LoadGame()
+        {
+            AsyncOperation loadOperations = SceneManager.LoadSceneAsync("Game");
+            while (!loadOperations!.isDone) yield return null;
+            
+            GameObject playerObj = GameObject.Find("Player");
+            Debug.Log(playerObj.name);
+        }
+    }
+}
